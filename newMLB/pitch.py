86f7b3735e5b913ed5eeb564be6ede9d8a77519e
apbadzioch <abadzioch@gmail.com>
2025-02-21 00:13:05 -0600

    new

--- newMLB/pitch.py
+++ newMLB/pitch.py
@@ -8,10 +8,35 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
-
 # Importing the datasets
 pitch = pd.read_csv('pitchStats.csv')
 batter = pd.read_csv('batterStats.csv')
 
-# Asking the user for the name of the pitcher
-pitcher = input("Enter the name of the pitcher you would like to analyze: ")
+# Filtering the data to only include the pitcher of interest
+def get_pitcher_data(pitcher):
+    pitcher_data = pitch[pitch['last_name, first_name'] == pitcher]
+    if pitcher_data.empty:
+        print("This pitcher is not in the dataset.");
+    else:                               
+        return pitcher_data
+
+def get_batter_data(batterr):
+    batter_data = batter[batter['last_name, first_name'] == batterr]
+    if batter_data.empty:
+        print("This batter is not in the dataset.");
+    else:
+        return batter_data
+
+
+def main():
+    pitcher = input("Enter the name of the pitcher you would like to analyze: ")    
+    batter = input("Enter the name of the batter you would like to analyze: ")
+
+    print(get_pitcher_data(pitcher))
+    print(get_batter_data(batter))
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
